cd2df4878c1ea61ce100d41295a69f7b6a7bb4c6
thaigialong <longtg.ce191181@gmail.com>
2025-12-15 18:34:33 +0700

    Thêm carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes,Route } from "react-router-dom";
+import HomeTemplate from "./templates/HomeTemplate";
 
 const App = () => {
   return (
 
-    <div>a</div>
+   <Routes>
+    <Route path="/" element={<HomeTemplate/>}> 
+
+    </Route>
+   </Routes>
  
   )
 };
---
+++ src/templates/HomeTemplate/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavbarComponent from '../../components/NavbarComponent'
+import FooterComponent from '../../components/FooterComponent'
+import CarouselComponent from './CarouselComponent/index.'
+
+const HomeTemplate = () => {
+  return (
+    <>
+      <NavbarComponent/>
+      <CarouselComponent/>
+      <FooterComponent/>
+    </>
+   
+  )
+}
+
+export default HomeTemplate
