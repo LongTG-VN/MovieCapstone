a780182b7313e0c7fc0cc98e2bbe46670e4d5da3
thaigialong <longtg.ce191181@gmail.com>
2025-12-15 17:36:04 +0700

    first commit

---
+++ src/service/HomeAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const homeAPI = axios.create({
+    baseURL: "https://movienew.cybersoft.edu.vn/api"
+})
+
+homeAPI.interceptors.request.use((config) => {
+    config.headers = {
+        ...config.headers,
+       TokenCybersoft : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MCIsIkhldEhhblN0cmluZyI6IjI5LzA1LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4MDAxMjgwMDAwMCIsIm5iZiI6MTc1MzAzMDgwMCwiZXhwIjoxNzgwMTYwNDAwfQ.KkGRtLpEsgoM4M_TapjOZIzvAwbay3QvXIwwN8XUqWk"
+    }
+  return config
+})
+
+
+export default homeAPI;
